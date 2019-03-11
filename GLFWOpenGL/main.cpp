4eb4d35ffcff779076abae15715c790e28c459b9
Azzen <6373020+Azzen@users.noreply.github.com>
2019-03-11 18:32:15 +0100

    Remove glew and glfw documentation

--- GLFWOpenGL/main.cpp
+++ GLFWOpenGL/main.cpp
@@ -11,6 +11,9 @@
 // shader.h
 #include "shader.h"
 
+// soil2 - texture application
+#include <SOIL2.h>
+
 const GLint WIDTH = 800, HEIGHT = 600;
 
 // Main 
@@ -49,35 +52,73 @@ int main()
 	
 	glViewport(0, 0, screenWidth, screenHeight);
 
+	// Enable texture alpha support
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+
+
 	Shader ourShader("core.vs", "core.frag");
 
 	// Setup vertex data and attributes pointers
 	GLfloat vertices[] =
 	{
 		// Positions           // Colors
-		0.5f, -0.5f, 0.0f,      1.0f, 0.0f, 0.0f,   // Bottom Right
-		-0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,   // Bottom Left
-		0.0f, 0.5f, 0.0f,       0.0f, 0.0f, 1.0f     // Top Right
+		0.5f, 0.5f, 0.0f,      1.0f, 0.0f, 0.0f,      1.0f, 1.0f,   // Top right
+		0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,      1.0f, 0.0f,   // Bottom right
+		-0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,      0.0f, 0.0f,   // Bottom left
+		-0.5f, 0.5f, 0.0f,     1.0f, 1.0f, 1.0f,      0.0f, 1.0f,   // Top Left 
+	};
+	GLuint indices[] =
+	{
+		0, 1, 3, // First triangle
+		1, 2, 3
 	};
 
 	// Creation of Vertex Buffer Object and Vertex Array Object
-	GLuint VBO, VAO;
+	GLuint VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 	glBindVertexArray(VAO);
+	glGenBuffers(1, &EBO);
+
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	
+
+	//Bind EBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
 	// Position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *)0);
 	glEnableVertexAttribArray(0);
 	// Color attribute | 6 * sizeof(GLfloat) -> 6 means 2 values for each vertex
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *)(3 * sizeof(GLfloat)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
+
+	// Texture coordinate attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
+	
 	// unbind VA
-	//glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+
+	// Load and create a texture
+	GLuint texture;
+	int width, height;
+
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	// Texture parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	// Set texture filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	//Window loop
 	while(!glfwWindowShouldClose(window))
